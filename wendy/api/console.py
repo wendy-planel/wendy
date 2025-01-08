de799215f92dd8788d80271fb67a5bc7e9e9d839
ylei <2457738122@qq.com>
2025-01-08 10:36:05 +0800

    feact: 新增接口读取全部控制台日志

--- wendy/api/console.py
+++ wendy/api/console.py
@@ -1,7 +1,12 @@
+from typing import List
+
+import asyncio
 import collections
 
+import aiodocker
 import structlog
 from fastapi import APIRouter, Body, Query
+from sse_starlette.sse import EventSourceResponse
 
 from wendy import agent, models
 from wendy.cluster import Cluster
@@ -38,7 +43,7 @@ async def command(
     "/logs/{id}",
     description="获取在线日志",
 )
-async def logs(
+async def tail_logs(
     id: int,
     tail: int = Query(default=50),
     world_name: str = Query(),
@@ -52,7 +57,6 @@ async def logs(
             container_name = world.container
     if docker_api is None:
         raise ValueError(f"world {world_name} not found")
-    # 获取日志
     tail += 1
     logs = collections.deque(maxlen=tail)
     async for line in agent.logs(docker_api, container_name):
@@ -60,3 +64,42 @@ async def logs(
         if len(logs) == tail:
             logs.popleft()
     return logs
+
+
+class LogFollow:
+    def __init__(self):
+        self._started = False
+        self.queue = asyncio.Queue()
+        self.tasks: List[asyncio.Task] = []
+
+    def __aiter__(self):
+        return self
+
+    async def read(self, queue: asyncio.Queue, deploy: models.Deploy):
+        cluster = Cluster.model_validate(deploy.cluster)
+        for world in cluster.world:
+            async with aiodocker.Docker(world.docker_api) as docker:
+                container = await docker.containers.get(world.container)
+                _iter = container.log(stdout=True, stderr=True, follow=True)
+                async for line in _iter:
+                    await queue.put(line)
+
+    async def aclose(self):
+        for task in self.tasks:
+            task.cancel()
+
+    async def __anext__(self):
+        if not self._started:
+            async for deploy in models.Deploy.all():
+                task = asyncio.create_task(self.read(self.queue, deploy))
+                self.tasks.append(task)
+            self._started = True
+        return await self.queue.get()
+
+
+@router.get(
+    "/logs",
+    description="获取所有控制台日志",
+)
+async def sse_logs():
+    return EventSourceResponse(LogFollow())
